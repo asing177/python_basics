d95c6c16895fa2aece9365c21daaa529838626ce
aditya singh <adityasingh27@hotmail.com>
2020-10-05 13:30:04 +0530

    calling mechanism

--- call_mechanism.py
+++ call_mechanism.py
@@ -9,4 +9,28 @@ def test(string):
 
 
 test(string)
-print("Outside :", string
+print("Outside :", string)
+
+
+def add(list):
+    list.append(50)
+    print("Inside : ", list)
+
+
+mylist = [10, 20, 30, 40]
+
+add(mylist)
+print("Outside :", mylist)
+
+
+
+a = [1,2,3]
+def foo(b):
+	b.append(4)
+	c = b[:]
+	c.append(11)
+	return c
+
+c = foo(a)
+print(a,c)
+
