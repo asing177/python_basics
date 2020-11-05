8feb2ea9cef88a286198ea1ed748da3096f8feca
aditya singh <adityasingh27@hotmail.com>
2020-11-05 18:49:55 +0530

    Mocking in python

--- mocking/tests/test_number.py
+++ mocking/tests/test_number.py
@@ -1,3 +1,4 @@
+import pytest
 from unittest import mock
 from mocking.guess import guess_number
 
@@ -6,3 +7,10 @@ from mocking.guess import guess_number
 def test_guess_number(mock_dice):
     mock_dice.return_value = 4
     assert guess_number(4) == True
+
+@pytest.mark.parametrize("_input, expected" , [(3, False), (4, True)])
+@mock.patch("mocking.guess.roll_dice")
+def test_guess_parameterized(mock_dice, _input ,expected):
+    mock_dice.return_value = 4
+    assert guess_number(_input) == expected
+
