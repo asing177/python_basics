3e4cd440ea2c82575cfb77227430c0ec934080cf
aditya singh <adityasingh27@hotmail.com>
2020-11-10 20:55:06 +0530

    Descriptors

---
+++ descriptors/basic.py
@@ -0,0 +1,22 @@
+class Descriptor(object):
+
+    def __init__(self, name=''):
+        self.name = name
+
+    def __get__(self, obj, objtype):
+        return "{} from class".format(self.name)
+
+    def __set__(self, obj, name):
+        if isinstance(name, str):
+            self.name = name
+        else:
+            raise TypeError("Name should be string")
+
+
+class Test(object):
+    name = Descriptor()
+
+
+g = Test()
+g.name = "hello"
+print(g.name)
