1da7c58b855c38c0003ac9f91808c13fa85958d5
aditya singh <adityasingh27@hotmail.com>
2020-11-11 12:49:53 +0530

    checking linked lists are merging

--- lists/check_list_merge.py
+++ lists/check_list_merge.py
@@ -10,15 +10,19 @@ def check_merge(lst1, lst2):
 
     flag = False
 
-    while ptr1!= None and ptr2 != None and \
-          ptr2.next != None and ptr1.next !=None:
+    while ptr2!= None and ptr2.next !=None:
+        ptr1 = lst1
 
-        ptr1 = ptr1.next
+        while ptr1!=None and ptr1.next != None:
+            if ptr2.data == ptr1.data:
+                flag = True
+                break
+            ptr1 = ptr1.next
+
         ptr2 = ptr2.next
 
-        if ptr1 == ptr2:
-            flag = True
-            break
+    if flag:
+        print("Lists are merging")
 
 
 
@@ -45,10 +49,10 @@ n5.next = n6 = Node('F')
 
 
 head2 = Node('P')
-head2.next = n6 = Node('Q')
-n6.next = n7 = Node('R')
-n7.next = n8 = Node('S')
-n8.next = n3
+head2.next = n7 = Node('Q')
+n7.next = n8 = Node('R')
+n8.next = n9 = Node('S')
+n9.next = n3
 
 print_list(head1)
 print_list(head2)
